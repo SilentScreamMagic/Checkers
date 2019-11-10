f201a038f9850db60a28ff5a73f0e45744520806
SilentScreamMagic <57537041+SilentScreamMagic@users.noreply.github.com>
2019-11-10 15:49:13 +0000

    setGrid update
    I redid the setGrid again. in order to work with it before I changed grid.length to 8 so now I have changed it back. its called bryanSetGrid

--- Grid.java
+++ Grid.java
@@ -16,7 +16,32 @@ public class Grid {
         grid=new Piece[size][size];
 
     }
-
+    private static void bryanSetGrid() {
+		int mid=grid.length/2;
+		int start;
+        for (int i = 0; i < mid-1; i++) {
+        	if(i%2==0){
+        		start =1;
+        	}else {
+        		start=0;
+        	}
+        	for (int j = start; j < grid.length; j=j+2) {
+                    grid[i][j] = new Piece(Piece.Role.R1);
+        	}
+        }
+    
+        for (int i = grid.length-1; i >= mid+1; i--) {
+        	if(i%2==0){
+        		start =1;
+        	}else {
+        		start=0;
+        	}
+        	for (int j = start; j < grid.length; j=j+2) {
+                    grid[i][j] = new Piece(Piece.Role.R2);
+               
+        	}
+        }
+	}
     public void setGrid(){
         int mid=grid.length/2;
 
